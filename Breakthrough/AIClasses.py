5c0b379660e7f7620aba268a614dbbd16450a36c
shmasmasnash <shmasmasnash@gmail.com>
2016-10-23 13:29:39 -0500

    added initial AI methods

--- Breakthrough/AIClasses.py
+++ Breakthrough/AIClasses.py
@@ -8,8 +8,101 @@ class Player(object):
     opponentWorkersCaptured = 0
     totalTimeTaken = 0
     numberOfMoves = 0
+    playerPieces = []
 
     @abc.abstractmethod
     def makeMove(self, board):
         """Analyzes the board state and returns a Movement for this player"""
         return
+
+# (* the minimax value of n, searched to depth d *)
+#  fun minimax(n: node, d: int): int =
+#    if leaf(n) or depth=0 return evaluate(n)
+#    if n is a max node
+#       v := L
+#       for each child of n
+#          v' := minimax (child,d-1)
+#          if v' > v, v:= v'
+#       return v
+#    if n is a min node
+#       v := W
+#       for each child of n
+#          v' := minimax (child,d-1)
+#          if v' < v, v:= v'
+#       return v
+
+class MinimaxPlayer(Player):
+    isOffensive = True
+
+    def __init__(self, isOffensive):
+        super.__init__()
+        self.isOffensive = isOffensive
+
+    def minimax(self, board, depth, isThisPlayerMoving, otherPlayer):
+        if board.isGameOver() is True or depth == 0:
+            if self.isOffensive:
+                self.offensiveEvaluate(board, otherPlayer)
+            else:
+                self.defensiveEvaluate(board, otherPlayer)
+        currentPlayer = None
+        if isThisPlayerMoving:
+            currentPlayer = self
+        else:
+            currentPlayer = otherPlayer
+        v = -999999
+        for pieceLocation in currentPlayer.playerPieces:
+            upValue = 0
+            if currentPlayer.color == Color.white:
+                upValue = 1
+            else:
+                upValue = -1
+            leftForward = Movement(pieceLocation, (pieceLocation[0] + upValue, pieceLocation[1] - 1))
+            forward = Movement(pieceLocation, (pieceLocation[0] + upValue, pieceLocation[1]))
+            rightForward = Movement(pieceLocation, (pieceLocation[0] + upValue, pieceLocation[1] + 1))
+            if board.isValidMove(currentPlayer, leftForward):
+                oldDestinationPiece = board.makeMovement(currentPlayer, leftForward)
+                vPrime = self.minimax(board, depth-1, not isThisPlayerMoving, otherPlayer)
+                if isThisPlayerMoving and vPrime > v:
+                    v = vPrime
+                elif not isThisPlayerMoving and vPrime < v:
+                    v = vPrime
+                board.undoMovement(currentPlayer, leftForward, oldDestinationPiece)
+            if board.isValidMove(currentPlayer, forward):
+                oldDestinationPiece = board.makeMovement(currentPlayer, forward)
+                vPrime = self.minimax(board, depth-1, not isThisPlayerMoving, otherPlayer)
+                if isThisPlayerMoving and vPrime > v:
+                    v = vPrime
+                elif not isThisPlayerMoving and vPrime < v:
+                    v = vPrime
+                board.undoMovement(currentPlayer, forward, oldDestinationPiece)
+            if board.isValidMove(currentPlayer, rightForward):
+                oldDestinationPiece = board.makeMovement(currentPlayer, rightForward)
+                vPrime = self.minimax(board, depth-1, not isThisPlayerMoving, otherPlayer)
+                if isThisPlayerMoving and vPrime > v:
+                    v = vPrime
+                elif not isThisPlayerMoving and vPrime < v:
+                    v = vPrime
+                board.undoMovement(currentPlayer, rightForward, oldDestinationPiece)
+
+    def offensiveEvaluate(self, board, otherPlayer):
+        total = 0
+        #total += 1 * len(self.playerPieces)
+        total -= 1 * len(otherPlayer.playerPieces)
+        for pieceLocation in self.playerPieces:
+            if self.color == Color.white:
+                total += 1 * pieceLocation[0]
+            else:
+                total += 1 * (board.dimension - (pieceLocation[0]+1))
+
+    def defensiveEvaluate(self, board, otherPlayer):
+        total = 0
+        total += 1 * len(self.playerPieces)
+        #total -= 1 * len(otherPlayer.playerPieces)
+        for pieceLocation in otherPlayer.playerPieces:
+            if otherPlayer.color == Color.white:
+                total -= 1 * pieceLocation[0]
+            else:
+                total -= 1 * (board.dimension - (pieceLocation[0]+1))
+
+
+class AlphaBetaPlayer(Player):
--- Breakthrough/BoardClasses.py
+++ Breakthrough/BoardClasses.py
@@ -47,6 +47,67 @@ class Board:
         isOffBoard = row < 0 or col < 0 or row >= self.dimension or col >= self.dimension
         return not isOffBoard
 
+    def isGameOver(self):
+        #Has either side has reached the opposing side's last row
+        for col in range(0, self.dimension):
+            checkPieceTop = self.getPiece(0, col)
+            if checkPieceTop is not None and checkPieceTop.color == Color.white:
+                return True
+            checkPieceBottom = self.getPiece(self.dimension - 1, col)
+            if checkPieceBottom is not None and checkPieceBottom.color == Color.black:
+                return True
+        #Check that each side has pieces left on the board
+        blackPiecesLeft = False
+        whitePiecesLeft = False
+        for row in range(0, self.dimension):
+            for col in range(0, self.dimension):
+                checkPiece = self.getPiece(row, col)
+                if checkPiece is not None:
+                    if checkPiece.color == Color.white:
+                        whitePiecesLeft = True
+                    else:
+                        blackPiecesLeft = True
+        if not blackPiecesLeft or not whitePiecesLeft:
+            return True
+
+    def makeMovement(self, playerMoving, movement):
+        #defendingPiece = self.getPiece(movement.destination[0], movement.destination[1])
+        #if defendingPiece is not None:
+            #playerMoving.opponentWorkersCaptured += 1
+        movingPiece = self.getPiece(movement.origin[0], movement.origin[1])
+        destinationPiece = self.getPiece(movement.destination[0], movement.destination[1])
+        self.setPiece(movement.destination[0], movement.destination[1], movingPiece)
+        self.setPiece(movement.origin[0], movement.origin[1], None)
+        return destinationPiece
+
+    def undoMovement(self, playerMoving, movement, destinationPiece):
+        movingPiece = self.getPiece(movement.destination[0], movement.destination[1])
+        self.setPiece(movement.origin[0], movement.origin[1], movingPiece)
+        self.setPiece(movement.destination[0], movement.destination[1], destinationPiece)
+
+    def isValidMove(self, playerMoving, movement):
+        playerColor = playerMoving.color
+        origin = movement.origin
+        destination = movement.destination
+        if not self.isPointOnBoard(origin[0], origin[1]) or not self.isPointOnBoard(destination[0], destination[1]):
+            return False
+        movingPiece = self.getPiece(origin[0], origin[1])
+        if movingPiece is None or movingPiece.color is not playerColor:
+            return False
+        # Pieces can only move forwards
+        if playerColor is Color.white and origin[0] > destination[0]:
+            return False
+        elif playerColor is Color.black and origin[0] < destination[0]:
+            return False
+        if abs(origin[1] - destination[1]) > 1:
+            return False
+        defendingPiece = self.getPiece(destination[0], destination[1])
+        if defendingPiece is not None:
+            if defendingPiece.color is playerColor:
+                return False
+            if origin[1] == destination[1]:
+                return False
+
 
 class Movement:
     origin = (0, 0)
